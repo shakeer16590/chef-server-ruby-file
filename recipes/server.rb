7cf586a566af0212944a28f0919b965cc50a223e
shakeer <shakeer16590@gmail.com>
2018-08-20 07:09:20 +0000

    Edited for Template file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,9 @@
 package 'apache2' do
   action :install
 end
-file '/var/www/html/index.html' do
- content "<h1> System configuration Details using ohai with Node Object </h1>
-	  IP-Address : #{node['ipaddress']} 
-	  HostName : #{node['hostname']} 
-	  Group : #{node['root_group']} 
-	  OperatingSystem : #{node['hostnamectl']['operating_system']}"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
 end
 service 'apache2' do
  action [ :enable, :restart]
