5d288b1031b6698615bf02daa3076a57025e3e56
shakeer <shakeer16590@gmail.com>
2018-08-22 14:48:17 +0000

    remote_file example

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,10 +2,15 @@
 package 'apache2' do
   action :install
 end
+
+remote_file '/var/www/html/pic.png' do
+  source 'https://vignette.wikia.nocookie.net/fantendo/images/6/6e/Small-mario.png/revision/latest?cb=20120718024112&format=original' 
+end
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   action :create
 end
+
 service 'apache2' do
  action [ :enable, :restart]
 end
