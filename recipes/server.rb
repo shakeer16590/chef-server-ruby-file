79923a34b85fd878050c3357fe1214e5a287232b
shakeer <shakeer16590@gmail.com>
2018-08-24 08:03:41 +0000

    notify sample

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,12 +6,17 @@ end
 remote_file '/var/www/html/pic.png' do
   source 'https://vignette.wikia.nocookie.net/fantendo/images/6/6e/Small-mario.png/revision/latest?cb=20120718024112&format=original' 
 end
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
+#end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   action :create
+  notifies :restart, 'service[apache2]', :immediately
 end
 
 service 'apache2' do
- action [ :enable, :restart]
+ action [ :enable, :start]
 end
 
